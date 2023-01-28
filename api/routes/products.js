36740f3050449d96878d469d25d536cfe5c9d44a
Kaan kripto <kaan_yldrm@live.com>
2023-01-28 20:08:40 +0300

    you something new every day

---
+++ api/routes/products.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'GET istekleri buradan isliyor'
+    });
+}); // sadece / olmasının sebebi /products yazarsak aradığı şey /products/products olacak
+
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'POST istekleri buradan isliyor'
+    });
+});
+
+
+router.get('/:productID', (req, res, next) => { // Burada /Products'un altına yeni alt birimler ekliyoruz
+    const id = req.params.productID;
+    if ( id === 'kaan') {
+        res.status(200).json ({
+            message: 'you have found me'        // /products/kaan girersen bu çıktıyı veriyor
+        });
+    } else {
+        res.status(200).json ({
+            message: 'Yeni bir Id girdin'       // /products/xxx yani herhangi bir şey girdiğinde bu çıktıyı veriyor
+        });
+    }
+});
+ 
+router.patch('/productID', (req, res, next) => {
+    res.status(200).json ({
+        message: 'Yeni Id yi guncelledin'
+    });
+});
+
+router.delete('/productID', (req, res, next) => {
+    res.status(200).json ({
+        message: 'Id yi sildin'
+    });
+});
+
+
+
+module.exports = router; // ile router artık server tarafından kullanılabilir hale geliyor.
--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express'); //express.js için
 
 const app = express(); // express'in çalıştırılması için
 
-app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'it is working'
-    });  //200 OK bilgisi almak için ve string formatında olması için json metodu ile yazıyoruz
-})  //gelen istekler buradan geçiyor
+const productRoutes = require('./api/routes/products');
+
+//app.use((req, res, next) => {
+//    res.status(200).json({
+//        message: 'it is working'
+//    });  //200 OK bilgisi almak için ve string formatında olması için json metodu ile yazıyoruz
+//})  //gelen istekler buradan geçiyor
+
+app.use('/products', productRoutes);    // /products'a gelen istekler productRoutes'u çağıracak oda 
+                                        // ./api/routes/products'u çağıracak
+
 
 module.exports = app;
