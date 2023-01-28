0b2e7ef951edfa1b09f720d5e8e066c906e78d20
Kaan kripto <kaan_yldrm@live.com>
2023-01-28 17:24:20 +0300

    some modifications, not really important

--- app-server.js
+++ app-server.js
@@ -1,17 +1,8 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('henlo \n');
-  res.end('this is a website');  
-});
+const port = process.env.port || 3000;
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-  console.log('this is shell btw');
-});
+const server = http.createServer();
 
+server.listen(port); // server, port'u dinleyerek createServer'a gönderdiğin şeyleri yapmaya başlar
