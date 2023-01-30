e396e8eb7c9a6f3803e2f5ec6b9b4014732a03bb
Kaan kripto <kaan_yldrm@live.com>
2023-01-30 23:12:17 +0300

    .

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Product = require('../Models/productModel');
+//productModel.js'den Product'u getirmek için
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
@@ -8,10 +11,24 @@ router.get('/', (req, res, next) => {
 }); // sadece / olmasının sebebi /products yazarsak aradığı şey /products/products olacak
 
 router.post('/', (req, res, next) => {
-    const product = {
+//    const product = {
+//        name: req.body.name,
+//        price: req.body.price
+//    };
+
+// alttaki MongoAtlas ile beraber çalışmasında
+// üstteki ise postman'dan giriş yaparken kullandığın
+
+const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
-        price: req.body.price
-    };
+        price: req.body.price        
+    });
+
+    product.save().then(result => {
+        console.log(result);
+    }).catch(err => console.log(err));
+    // product bilgilerini kaydeder ve sonucunu çıkartır, hata verdiyse hatayı çıkartır.
     res.status(201).json({
         message: 'POST istekleri buradan isliyor',
         createdProduct: product
--- app.js
+++ app.js
@@ -8,8 +8,14 @@ const mongoose = require('mongoose');
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
-mongoose.connect('mongodb+srv://Henlo1here:' + process.env.MONGO_ATLAS_PWD + '@henlo.2m8afdg.mongodb.net/?retryWrites=true&w=majority', {
-    useMongoClient: true
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://Henlo1here:" + process.env.MONGO_ATLAS_PWD + "@henlo.2m8afdg.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
 });
 
 //app.use((req, res, next) => {
